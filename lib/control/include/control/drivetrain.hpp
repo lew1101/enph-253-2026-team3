04cb631808729abe4fe7477ba29f86a3efa13918
Tri Cao <tricaotrangminh@gmail.com>
2026-07-02 11:12:51 -0700

    moved the initialization and MCPWM stuff into drivetrain init()

--- lib/control/include/control/drivetrain.hpp
+++ lib/control/include/control/drivetrain.hpp
@@ -5,11 +5,12 @@ namespace control {
 class Drivetrain {
     public:
         struct Config{
-            mcpwm_timer_handle_t timer = nullptr;
+            mcpwm_timer_handle_t timer_0 = nullptr;
+            mcpwm_timer_handle_t timer_1 = nullptr;
 
             driver::DCDriver::Config front_right_motor_config;
-            driver::DCDriver::Config front_left_motor_config;
             driver::DCDriver::Config back_right_motor_config;
+            driver::DCDriver::Config front_left_motor_config;
             driver::DCDriver::Config back_left_motor_config;
         };
 
@@ -30,6 +31,10 @@ class Drivetrain {
         double wheel_radius = 0.05; // meters
         double wheel_base = 0.2; // ditance between front and rear (meters)
         double track_width = 0.2; // distance between left and right (meters)
+
+        int timer_resolution_hz = 1000000;
+        int PWM_frequency_hz = 10000;
+        float period_tick = (timer_resolution_hz / (PWM_frequency_hz * 2));
 
         driver::DCDriver front_right_motor;
         driver::DCDriver front_left_motor;
