ac0e411c81d2909d9047b09a17e30f2e1bef6d59
Tri Cao <tricaotrangminh@gmail.com>
2026-07-16 12:01:25 -0700

    switched the order for min percentage and clamp percentage

--- lib/actuators/include/actuators/dc_driver.hpp
+++ lib/actuators/include/actuators/dc_driver.hpp
@@ -24,8 +24,8 @@ class DCDriver {
             int group_id = 0;
             mcpwm_timer_handle_t timer = nullptr;
             uint32_t period_ticks = 0;
-            float clamp_percentage = 100.0f;
             float min_percentage = 0.0f;
+            float clamp_percentage = 100.0f;
         };
 
         DCDriver() = default;
