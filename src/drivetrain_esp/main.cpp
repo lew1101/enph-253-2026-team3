224ea0f2f5865be46ddee694faa65723b88ff4e9
Kenneth Lew <kennethlew001@gmail.com>
2026-07-17 20:18:45 -0700

    add entrypoint for drivetrain_esp

--- src/drivetrain_esp/main.cpp
+++ src/drivetrain_esp/main.cpp
@@ -4,8 +4,17 @@
 #include "tasks/imu.hpp"
 #include "tasks/uart.hpp"
 
-void setup() {
+TaskHandle_t uart_tx_handle;
+TaskHandle_t uart_rx_handle;
+TaskHandle_t drive_handle;
+TaskHandle_t imu_sensor_handle;
+TaskHandle_t tape_handle;
 
+void setup() {
+    start_uart_tasks(&uart_tx_handle, &uart_rx_handle);
+    start_drive_task(&drive_handle);
+    start_imu_task(&imu_sensor_handle);
+    start_tape_sense_task(&tape_handle);
 
     vTaskDelete(nullptr);
 }
