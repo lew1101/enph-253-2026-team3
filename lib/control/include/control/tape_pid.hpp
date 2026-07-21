f92851c687075ce747ad7a4fbbd012aab9e54057
Tri Cao <tricaotrangminh@gmail.com>
2026-07-20 18:24:08 -0700

    added a way to clamp the derivative error so it doesn't explode

--- lib/control/include/control/tape_pid.hpp
+++ lib/control/include/control/tape_pid.hpp
@@ -7,11 +7,13 @@ class TapePID : public PID {
         : PID(kp, ki, kd), _d_alpha(d_alpha), _filtered_derivative(0.0f) {}
 
     float update(float ref, float meas, float dt_s) override;
+    void set_derivative_error (float clamp_error) { _d_clamp_error = clamp_error; }
     void reset() override;
 
     protected:
     float _d_alpha = 0.1f; // Low-pass filter coefficient for derivative term
     float _filtered_derivative = 0.0f;
+    float _d_clamp_error = 5.0f; // Clamp error for derivative term to avoid spikes
 };
 
 }// namespace control
